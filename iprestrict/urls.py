ff82501f54f4fb3a00c30f56079463b701cbb53d
Roman <roman.oxenuk@gmail.com>
2016-08-18 15:05:21 +0300

    Delete app_name from urls.py

--- iprestrict/urls.py
+++ iprestrict/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import test_rules_page, test_match, reload_rules
 from .views import move_rule_up, move_rule_down
 
-app_name = "iprestrict"
+
 urlpatterns = [
     url(r'^$', test_rules_page),
     url(r'^move_rule_up/(?P<rule_id>\d+)[/]?$', move_rule_up),
